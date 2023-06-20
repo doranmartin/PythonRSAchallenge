444bf91394aa017b933f791abd22171a7a8cc0cb
doranmartin <doranmartin1994@gmail.com>
2023-06-20 16:25:38 -0400

    something wrong with challenge files?

--- RSAchallenge.py
+++ RSAchallenge.py
@@ -1,4 +1,6 @@
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import base64
 
 def gcd(a, b):
     while b != 0:
@@ -35,4 +37,34 @@ vulnerable_keys = [7, 93, 9, 44, 29, 82, 34, 80, 58, 71, 60, 97]
 #     print(str(vulnerable_keys[i + 1]) + ": p = " + str("{:.0f}".format(file_2 / factor)) + " | q = " + str(factor))
 #     i += 2
 
+decrypted_data = ''
 
+# for key_num in vulnerable_keys:
+#     key = RSA.import_key(open('private_keys/' + str(key_num) + '_priv.pem').read())
+#     print('private_keys/' + str(key_num) + '_priv.pem')
+#     cipher = PKCS1_OAEP.new(key)
+#     print('challenge/' + str(key_num) + '.bin')
+#     ciphertext = open('challenge/' + str(key_num) + '.bin')
+#     decrypted_data += cipher.decrypt(ciphertext)
+
+# with open('outputfile.txt', 'w') as decrypted_file:
+#     decrypted_file.write(decrypted_data)
+
+# for key_num in vulnerable_keys:
+#     key = RSA.import_key(open('private_keys/93' + '_priv.pem').read())
+#     print('private_keys93' + '_priv.pem')
+#     cipher = PKCS1_OAEP.new(key)
+#     print('challenge/93' + '.bin')
+#     ciphertext = open('challenge/93' + '.bin')
+#     decrypted_data += cipher.decrypt(ciphertext)
+
+# with open('outputfile.txt', 'w') as decrypted_file:
+#     decrypted_file.write(decrypted_data)
+
+binary_data = open('challenge/1.bin').read()
+
+base64_data = base64.b64decode(binary_data)
+
+base64_string = base64_data.decode("utf-8")
+
+print(base64_string)
