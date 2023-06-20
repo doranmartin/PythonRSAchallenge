859e1fe965a4a9504b6526db255e33a1e6a9051d
doranmartin <doranmartin1994@gmail.com>
2023-06-20 11:57:18 -0400

    first iteration completed. found common factors between keys

--- RSAchallenge.py
+++ RSAchallenge.py
@@ -1,10 +1,19 @@
 from Crypto.PublicKey import RSA
 
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+n_list = []
+for file_number in range(1, 101):
+    pem_file = open("challenge/" + str(file_number) + ".pem").read()
+    key_modulus = RSA.import_key(pem_file)
+    n_list.append(key_modulus.n)
+
+common_factors = []
 for i in range(100):
-    for j in range(100):
-        x = 5
-
-pem1 = open("challenge/1.pem").read()
-k1 = RSA.importKey(pem1)
-print(k1.n)
-
+    for j in range(i + 1, 100):
+        gcd_output = gcd(n_list[i], n_list[j])
+        if gcd_output != 1:
+            print(str(i + 1) + ".pem with " + str(j + 1) + ".pem " + str(gcd_output))
